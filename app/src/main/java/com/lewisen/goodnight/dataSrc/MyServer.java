5643e951613761425c1f11e933503bdb3178ebc6
Liukeshen1221 <592614804@qq.com>
2016-04-09 10:17:22 +0800

    Server

--- app/src/main/java/com/lewisen/goodnight/dataSrc/MyServer.java
+++ app/src/main/java/com/lewisen/goodnight/dataSrc/MyServer.java
@@ -2,18 +2,18 @@ package com.lewisen.goodnight.dataSrc;
 
 /**
  * 存放与服务器相关的数据
- * 本项目仅仅用来一起学习交流，请勿向服务器捣蛋啊。。。。
+ *
  * @author Lewisen
  */
 public class MyServer {
-    public static final String URL = "xx";
-    public static final String HOME_PAGE = URL + "xx";
-    public static final String ARTICLE_PAGE = URL + "xx";
-    public static final String PICTURE_PAGE = URL + "xx";
-    public static final String SECOND_IMAGE = URL + "xx";
-    public static final String PICTURE_URL = URL + "xx";
-    public static final String SHARE_URL = URL + "xx";
-    public static final String APP_ICON = PICTURE_URL + "xx";
-    public static final String MAX_ID = URL + "xx";
-    public static final String LIKE = URL + "xx";
+    public static final String URL = "http://42.96.149.128:8080/goodNightServer/";
+    public static final String HOME_PAGE = URL + "HomePageServlet?id=";
+    public static final String ARTICLE_PAGE = URL + "ArticlePageServlet?id=";
+    public static final String PICTURE_PAGE = URL + "PicturePageServlet?id=";
+    public static final String SECOND_IMAGE = URL + "SecondImageServlet";
+    public static final String PICTURE_URL = URL + "PictureServlet?path=";
+    public static final String SHARE_URL = URL + "ShareContent?";
+    public static final String APP_ICON = PICTURE_URL + "C://pic//app.jpg";
+    public static final String MAX_ID = URL + "MaxIdManageServlet?order=get";
+    public static final String LIKE = URL + "LikeServlet";
 }
